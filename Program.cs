a4ee57cfda822acded2be8ae177e537ae3a75b1c
Olga <miarik@yandex.ru>
2022-08-15 09:36:18 -0400

    Lection3

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Таблица умножения");
+
+string text = "Какое Сегодня красивое утро";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue){
+            result = result + $"{newValue}";
+        }
+        else{
+            result = result + $"{text[i]}";
+        }
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '-');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'К', 'к');
+Console.WriteLine(newText);
